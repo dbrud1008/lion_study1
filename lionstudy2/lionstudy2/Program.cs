ec83f64138f23a836c79fe01ab8e6015b2e0c022
youKyoung <qw711127@naver.com>
2025-02-21 10:23:05 +0900

    study2

---
+++ lionstudy2/lionstudy2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lionstudy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age; //정수형 변ㄴ수
+            age = 10; //변수에 값 저장
+
+            Console.WriteLine(age); //변수에 저장된 값을 출력
+        }
+    }
+}
