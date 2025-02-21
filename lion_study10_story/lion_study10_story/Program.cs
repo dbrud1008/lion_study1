fee3c41fdb73cf770fbb9c01f2c765c90c8b7fc8
youKyoung <qw711127@naver.com>
2025-02-21 17:05:00 +0900

    study

---
+++ lion_study10_story/lion_study10_story/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lion_study10_story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter를 입력하면 스토리가 시작됩니다. ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("로딩중");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear();
+            Console.WriteLine("로딩중.");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear();
+            Console.WriteLine("로딩중..");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear();
+            Console.WriteLine("로딩중...");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear();
+
+            Console.WriteLine("아침이 되었다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("(*^ω^*)/ 안녕!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("(*^ω^*)/ 난 너의 행복이! 오늘도 활기차게!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Q_Q.. 안녕.. 난 슬픔이.. 비가 오는 날처럼 슬픈 건 없지");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("(-_-#) " +
+                "난 버럭이야, " +
+                "아~ 기분이 꿀꿀한 게 화가 치솟아!");
+
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("(*^ω^*)/ 버럭아 그럴때일수록 좋은 생각을 해~!");
+
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("(-_-#) 화나는 일 밖에 없는데 좋은 생각이라니! 제정신이야?!");
+
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Q_Q.. 얘들아.. 싸우지마.. 싸우면 슬퍼");
+
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("오늘도 왠지 시끄러운 하루가 될 것 같다.");
+        }
+    }
+}
---
+++ lionstudy9_todaytask1/lionstudy9_todaytask1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lionstudy9_todaytask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            //게임스토리1
+
+            Console.WriteLine("Enter를 입력하면 스토리가 시작됩니다. ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("로딩중");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear();
+            Console.WriteLine("로딩중.");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear();
+            Console.WriteLine("로딩중..");
+            Thread.Sleep(1000); //1000 1초다 (1000밀리초)
+            Console.Clear();
+            Console.WriteLine("로딩중...");
+            Console.Clear();
+
+            Console.WriteLine("[어둠]");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("'나'는 어둠을 좋아한다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어둠은 나에게 숨겨진 이야기의 세계로 인도한다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어둠은 자극적이고, 종종 두려운 이야기들로 가득하다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("'내'가 혼자 느끼는 감정, 그러나 많은 사람들이 여전히 그 어둠을 즐긴다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("미디어는 이 어둠을 쉽게 접할 수 있는 통로이다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("다른 사람들은 내가 어둠 속에 있는지, 즐기고 있는지 모른다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어둠은 '내'가 아닌 다른 존재가 되는 것 같기 때문이다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("[속보]");
+            Console.WriteLine("22살의 00연예인, 악플에 시달려 사망...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("'나'의 댓글: '어린 나이에 너무 안타깝네요. 고인의 명복을 빕니다.'");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("[속보]");
+            Console.WriteLine("00연예인, 팬을 기만했다? 라이브에서의 정황...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("'나'의 댓글: '와; 그럴 줄 알았다, 나가 죽어라.'");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("오늘도 정말 재밌는 어둠이었다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("내일은 어떠한 소식이 나를 기다릴까?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("역시 나는");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어둠에 끌린다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어둠은 나에게 자유와 동시에 두려움을 안겨준다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("'나'는 어둠을 좋아한다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("End.");
+
+        }
+    }
+}
