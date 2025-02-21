a479d5d71123b9749c797187bfeb718e2ef1c35b
youKyoung <qw711127@naver.com>
2025-02-21 10:41:30 +0900

    study3

---
+++ lionstudy3/lionstudy3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lionstudy3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //리터럴 : 코드에서 고정된 값을 의미
+            int hp = 100; //정수형 리터럴
+            double att = 56.7; //소수점인 실수형 리터럴
+            char st = 'S';
+            string name = "Ace";
+
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(st);
+            Console.WriteLine(name);
+        }
+    }
+}
