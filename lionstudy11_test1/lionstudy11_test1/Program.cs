8541bc67cc3bada6052a713be16002f8de675041
youKyoung <qw711127@naver.com>
2025-02-24 10:32:29 +0900

    study

---
+++ lionstudy11_test1/lionstudy11_test1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lionstudy11_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!"); // 문자열 출력
+
+            //Console.Write("이름을 입력해주세요.");
+
+            Console.WriteLine("게임을 시작하시겠습니까? (Y/N): ");
+
+            // 특정 키 입력 대기
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true); // true를 주면 입력한 문자를 숨기는 옵션
+
+            // 입력한 키에 따라 행동
+            if (keyInfo.Key == ConsoleKey.Y) // Y 키를 체크
+            {
+                Console.WriteLine("\n게임 시작합니다!");
+                // 게임 시작 로직
+            }
+            else if (keyInfo.Key == ConsoleKey.N) // N 키를 체크
+            {
+                Console.WriteLine("\n게임을 종료합니다.");
+                // 게임 종료 로직
+            }
+            else
+            {
+                Console.WriteLine("\n잘못된 입력입니다. Y 또는 N을 입력하세요.");
+            }
+        }
+    }
+}
---
+++ lionstudy_12/lionstudy_12/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lionstudy_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////이진수를 정수로 변환
+            //Console.Write("2진수를 입력하세요: ");
+            //string binaryInput = Console.ReadLine(); //문자열을 입력 받는다
+            //int decimalValue = Convert.ToInt32(binaryInput, 2); //2진수 -> 10진수 변환
+
+            ////정수를 이진수로 변환
+            //string binaryOutput = Convert.ToString(decimalValue, 2); //10진수 -> 2진수 변환
+
+            //Console.WriteLine($"입력한 이진수: {binaryInput}");
+            //Console.WriteLine($"10진수로 변환: {decimalValue}");
+            //Console.WriteLine($"다시 이진수로 변환: {binaryOutput}");
+
+
+            ////var를 사용하여 변수 선언
+            //var name = "Alice"; //문자열로 추론
+            //var age = 25; //정수로 추론
+            //var isStudent = true; //논리값으로 추론
+
+            //Console.WriteLine($"이름: {name}, 나이: {age}, 학생 여부: {isStudent}");
+
+
+            ////default 키워드 사용한 기본값 설정
+            //int defaultInt = default; //기본값: 0
+            //string defaultString = default; //기본값: null'
+
+            //bool defaultBool = default; //기본값: false
+
+            //Console.WriteLine($"정수 기본값: {defaultInt}");
+            //Console.WriteLine($"문자열 기본값: {defaultString}");
+            //Console.WriteLine($"논리값 기본값: {defaultBool}");
+
+
+            //연산자 활용
+            //int a = 5, b = 3;
+            //int sum = 0;
+
+            //sum = a + b; //산술 연산자 사용
+            //Console.WriteLine($"합: {sum}"); //출력 8 
+
+            //sum = a - b; //산술 연산자 사용
+            //Console.WriteLine($"합: {sum}"); //출력 8
+
+            //sum = a * b; //산술 연산자 사용
+            //Console.WriteLine($"합: {sum}"); //출력 8
+
+            //sum = a / b; //산술 연산자 사용 -> 정수이기 때문에 소수점 불가능, 몫만 나옴
+            //Console.WriteLine($"합: {sum}"); //출력 8
+
+            //sum = a % b; //산술 연산자 사용 -> 나머지 값
+            //Console.WriteLine($"합: {sum}"); //출력 8
+
+
+            ////짝수 홀수 판별
+            //int number = 7;
+            //int sum = 0;
+            //sum = number % 2; // 0 나오면 짝수, 1 나오면 홀수 --> 랜덤 몇개 뽑기 등 유용한 게 많음 
+            //Console.WriteLine("짝수 홀수 판별: " + sum);
+
+
+            bool isEqual = false; // 거짓 0
+            int a = 5;
+            int b = 5;
+
+            //관계형 연산자
+            isEqual = (a == b); //a랑 b랑 같은가?
+
+            Console.WriteLine("같은가? " + isEqual);
+
+
+        }
+    }
+}
